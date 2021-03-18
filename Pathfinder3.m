f307be5359cd9d1b48fa2584041666d0b5dc9d77
realmisch <79525520+realmisch@users.noreply.github.com>
2021-03-18 14:30:05 -0400

    Revert "Annotation"
    This reverts commit 24d7cde1fef3ca11d2594b387bae8a74ee18e2ce.

--- Pathfinder3.m
+++ Pathfinder3.m
@@ -1,19 +1,17 @@
 function [Path] = Pathfinder3(shade)
-    %Pathfinder Version 3.0
-    %Now with shades!
-    %James Nellis 2021
+    
     [ylim,xlim]=size(shade);
-
-    archive=ones([ylim,xlim]);      %Tracks which pixels have been checked
-    sumcheck=sum(archive,'all');    %Checks algorithm progress
-    i=1;                            %x-coordinate
-    j=1;                            %y-coordinate
-    count=1;                        %Path coordinate pair number
-    xlist=[];                       %List of x-coordinates to visit
-    ylist=[];                       %List of y-coordinates to visit
-    radius=1;                       %Box-search radius
-
-    %No need to visit whitespace, sets all whitespace to 'checked'
+    
+    archive=ones([ylim,xlim]);
+    sumcheck=sum(archive,'all');
+    i=1;
+    j=1;
+    count=1;
+    xlist=[];
+    ylist=[];
+    log=[1,1];
+    radius=1;
+    
     for w=1:xlim
         for v=1:ylim
             if shade(v,w)==0
@@ -21,13 +19,10 @@ function [Path] = Pathfinder3(shade)
             end
         end
     end
-
-    %Main Pathfinder
+    
     while sumcheck~=0
         disp(ceil(sumcheck*0.001))
-        %MrHouse is in charge of gambling
         mrhouse=randi([1,100]);
-        %Creating the path, with probabilities for non-blackspace pixels
         if archive(j,i)==1 && shade(j,i)==3
             xlist(count)=i;
             ylist(count)=j;
@@ -42,27 +37,23 @@ function [Path] = Pathfinder3(shade)
             count=count+1;
         end
         archive(j,i)=0;
-        %Finding the next pixel using a box-search algorithm
+        
         while archive(j,i)==0
             for m=-radius:1:radius
                 xindex=i+m;
                 yindex=j+m;
                 if xindex<=0 || xindex>xlim || yindex<=0 || yindex>ylim
-                    %We have fallen off the Flat Earth and found a turtle
+
                 elseif i-radius>0 && archive(yindex,i-radius)==1
-                    %Searching Right Side of the box
                     i=i-radius;
                     j=yindex;
                 elseif i+radius<=xlim && archive(yindex,i+radius)==1
-                    %Searching Left Side of the box
                     i=i+radius;
                     j=yindex;
                 elseif j-radius>0 && archive(j-radius,xindex)==1
-                    %Searching Bottom of the box
                     i=xindex;
                     j=j-radius;
                 elseif j+radius<=ylim && archive(j+radius,xindex)==1
-                    %Searching Top of the box
                     i=xindex;
                     j=j+radius;
                 end
@@ -70,8 +61,6 @@ function [Path] = Pathfinder3(shade)
                     break
                 end
             end
-            %If a pixel is found, reset algorithm, if not, look harder and
-            %farther
             if archive(j,i)==0
                 radius=radius+1;
             else
@@ -79,11 +68,9 @@ function [Path] = Pathfinder3(shade)
             end
         end
         sumcheck=sum(archive,'all');
-        %"Good Enough" Approximation
         if sumcheck<500
             sumcheck=0;
         end
     end
-    %Return the Path
     Path=cat(1,xlist,ylist);
 end
